177d53fbc15085d5447e43eef71b8df87409b782
JechtSh0t <jechtshot91@gmail.com>
2022-12-12 11:57:41 -0500

    [Feature] Adjust rules for 2022

--- TurboManChallenge/TurboManChallenge/Sources/Models/EventBlueprint.swift
+++ TurboManChallenge/TurboManChallenge/Sources/Models/EventBlueprint.swift
@@ -25,20 +25,20 @@ extension EventBlueprint {
         EventBlueprint(isRepeatable: true, playersRequired: 0, text: "Shit! A flat tire. The leader can not drink until the next round."),
         
         EventBlueprint(isRepeatable: true, playersRequired: 1, text: "You found the last Turbo Man at Toy Land. %@ gains one point."),
-        EventBlueprint(isRepeatable: true, playersRequired: 1, text: "Dementor catches %@ and now he must take a shot of his choice in order to escape his lair."),
+        EventBlueprint(isRepeatable: true, playersRequired: 1, text: "Dementor catches %@ and now he must take a swig of Jones soda in order to escape his lair."),
         
         EventBlueprint(isRepeatable: true, playersRequired: 1, text: "The toy store opens in two minutes. %@ must land a single flip cup on the first try in order to enter the store and receive one point."),
         EventBlueprint(isRepeatable: true, playersRequired: 1, text: "You’ve been caught driving around traffic on the way to your son’s karate class. Officer Hummel detains %@ and he cannot drink until the start of the next round."),
-        EventBlueprint(isRepeatable: true, playersRequired: 1, text: "You ruined your son’s Christmas, take one heavy beer and finish it before drinking anything else. Here’s to you %@!"),
+        EventBlueprint(isRepeatable: true, playersRequired: 1, text: "A bomb? Swap one of your beers with someone else. Here’s to you %@!"),
         EventBlueprint(isRepeatable: true, playersRequired: 1, text: "Oh no!! %@ has been caught by the Demon Team. Lose one point."),
         
         EventBlueprint(isRepeatable: true, playersRequired: 1, text: "Mr. Ponytail man says %@ can't sink this cup. Take a single shot for one point."),
         
-        EventBlueprint(isRepeatable: true, playersRequired: 1, text: "Jingle bells batman smells. It's the mob against %@."),
         EventBlueprint(isRepeatable: true, playersRequired: 1, text: "%@ found Ted on the roof. Take three shots to knock his ass off with a quarter."),
         
         EventBlueprint(isRepeatable: true, playersRequired: 2, text: "They have one more Turbo Man at the radio station on Wabasha Street. %@ and %@ have a chug off to see who gets there first. The winner gets one point."),
         
-        EventBlueprint(isRepeatable: false, playersRequired: 1, text: "%@ goes to try one of Liz’s cookies and burns their hand. Accept the One Chip Challenge or lose three points as forfeit.")
+        EventBlueprint(isRepeatable: true, playersRequired: 1, text: "He got two!! %@ can steal a beer from anybody!"),
+        EventBlueprint(isRepeatable: true, playersRequired: 2, text: "He got two!! %@ can steal a beer from %@!")
     ]
 }
